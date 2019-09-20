217870cb6bf28fa317d24e86539a160131ae8aaa
haoyuF996 <50822651+haoyuF996@users.noreply.github.com>
2019-09-20 10:56:10 +0800

    Add files via upload

--- 0920.py
+++ 0920.py
@@ -1,15 +1,14 @@
 def pyramid(): 
-    char = input('An character:')
-    num = int(input('An odd number:'))
+    char,num = input('An character:'),int(input('An odd number:'))
     for i in range(1,num+2,2):
-        print(' '*int((num-i)/2)+char*i)
+        print(' '*((num-i)//2)+char*i)
 def stupid_pyramid():
     symbol = input('Symbol:')#string
     Maxnumber = int(input('Maxnumber:'))#integer
     while (-1)**Maxnumber != -1:
         print('An odd number is expected.')
         Maxnumber = int(input('Maxnumber:'))
-    NumberOfSpace = int((Maxnumber-1)/2)#integer
+    NumberOfSpace = (Maxnumber-1)//2#integer
     NumberOfSymbol = 1#integer
     while NumberOfSymbol<=Maxnumber:
         for i in range(NumberOfSpace):
@@ -19,8 +18,17 @@ def stupid_pyramid():
         print()
         NumberOfSpace-=1
         NumberOfSymbol+=2
+def hollow_pyramid():
+    char,num = input('An character:'),int(input('An odd number:'))
+    print(' '*int((num-1)/2)+char)
+    for i in range(3,num,2):
+        print(' '*((num-i)//2)+char+' '*(i-2)+char)
+    print(char*num)
 if __name__ == "__main__":
-    if input('Stupid or not?')=='stupid':
+    choice = input('Stupid,Normal or Hollow:(S/N/H)')
+    if choice == 'S':
         stupid_pyramid()
-    else:
+    elif choice == 'N':
         pyramid()
+    else:
+        hollow_pyramid()
